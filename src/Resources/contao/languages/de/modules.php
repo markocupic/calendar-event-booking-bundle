827dba9d35821e5350a95a89af6e593be91b2ea4
Marko Cupic <m.cupic@gmx.ch>
2020-08-21 21:16:47 +0200

    Update modules.php

--- src/Resources/contao/languages/de/modules.php
+++ src/Resources/contao/languages/de/modules.php
@@ -9,6 +9,6 @@
  */
 
 // Frontend modules
-$GLOBALS['TL_LANG']['FMD']['eventbooking'] = ['Event-Buchungsformular'];
-$GLOBALS['TL_LANG']['FMD']['unsubscribefromevent'] = ['Event-Abmeldeformular'];
-$GLOBALS['TL_LANG']['FMD']['calendar_event_booking_member_list'] = ['Event-Mitglieder-Auflistung', 'Listen Sie in Verbindung mit einem Event Reader die angemeldeten Mitglieder eines Events auf.'];
+$GLOBALS['TL_LANG']['FMD']['calendar_event_booking_event_booking_module'] = ['Event-Buchungsformular'];
+$GLOBALS['TL_LANG']['FMD']['calendar_event_booking_unsubscribe_from_event_module'] = ['Event-Abmeldeformular'];
+$GLOBALS['TL_LANG']['FMD']['calendar_event_booking_member_list_module'] = ['Event-Mitglieder-Auflistung', 'Listen Sie in Verbindung mit einem Event Reader die angemeldeten Mitglieder eines Events auf.'];
