508c17231e0de7e69070447a000f3625b6b92f9a
unknown <Lenovo@LENOVOT450.VETKO.local>
2021-09-27 23:12:38 +0200

    Update Plugin.php

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -19,6 +19,7 @@ use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Markocupic\CalendarEventBookingBundle\MarkocupicCalendarEventBookingBundle;
 
 class Plugin implements BundlePluginInterface
 {
