6d69e04d08e9d5abb8849b7e680b1ff3869d2ed8
Marko Cupic <m.cupic@gmx.ch>
2022-10-31 10:30:45 +0100

    Add PluginTest.php

---
+++ tests/ContaoManager/PluginTest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Calendar Event Booking Bundle.
+ *
+ * (c) Marko Cupic 2022 <m.cupic@gmx.ch>
+ * @license MIT
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ * @link https://github.com/markocupic/calendar-event-booking-bundle
+ */
+
+namespace Markocupic\CalendarEventBookingBundle\Tests\ContaoManager;
+
+use Contao\CalendarBundle\ContaoCalendarBundle;
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use PHPUnit\Framework\TestCase;
+use Markocupic\CalendarEventBookingBundle\ContaoManager\Plugin;
+use Markocupic\CalendarEventBookingBundle\MarkocupicCalendarEventBookingBundle;
+
+class PluginTest extends TestCase
+{
+    /**
+     * Test Contao manager plugin class instantiation.
+     */
+    public function testInstantiation(): void
+    {
+        $this->assertInstanceOf(Plugin::class, new Plugin());
+    }
+
+    public function testReturnsTheBundles(): void
+    {
+        $parser = $this->createMock(ParserInterface::class);
+
+        /** @var BundleConfig $config */
+        $config = (new Plugin())->getBundles($parser)[0];
+
+        $this->assertInstanceOf(BundleConfig::class, $config);
+        $this->assertSame(MarkocupicCalendarEventBookingBundle::class, $config->getName());
+        $this->assertSame([ContaoCalendarBundle::class, ContaoCoreBundle::class], $config->getLoadAfter());
+    }
+}
