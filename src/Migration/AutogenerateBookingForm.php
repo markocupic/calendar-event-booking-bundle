478528426a8a23ff9e18bfcf85f599bb214b8fe6
Marko Cupic <m.cupic@gmx.ch>
2022-10-31 13:42:38 +0100

    Store sample form data in a YAML object

--- src/Migration/AutogenerateBookingForm.php
+++ src/Migration/AutogenerateBookingForm.php
@@ -19,8 +19,12 @@ use Contao\CoreBundle\Framework\Adapter;
 use Contao\CoreBundle\Framework\ContaoFramework;
 use Contao\CoreBundle\Migration\AbstractMigration;
 use Contao\CoreBundle\Migration\MigrationResult;
+use Contao\FormFieldModel;
+use Contao\FormModel;
+use Contao\Input;
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Exception;
+use Symfony\Component\Yaml\Yaml;
 
 class AutogenerateBookingForm extends AbstractMigration
 {
@@ -28,17 +32,11 @@ class AutogenerateBookingForm extends AbstractMigration
     private Connection $connection;
     private string $projectDir;
 
-    // Adapters
-    private Adapter $controller;
-
     public function __construct(ContaoFramework $framework, Connection $connection, string $projectDir)
     {
         $this->framework = $framework;
         $this->connection = $connection;
         $this->projectDir = $projectDir;
-
-        // Adapters
-        $this->controller = $this->framework->getAdapter(Controller::class);
     }
 
     /**
@@ -46,7 +44,7 @@ class AutogenerateBookingForm extends AbstractMigration
      */
     public function shouldRun(): bool
     {
-        $schemaManager = $this->connection->getSchemaManager();
+        $schemaManager = $this->connection->createSchemaManager();
 
         // If the database table itself does not exist we should do nothing
         if (!$schemaManager->tablesExist(['tl_form'])) {
@@ -71,7 +69,7 @@ class AutogenerateBookingForm extends AbstractMigration
     }
 
     /**
-     * @throws Exception
+     * @return MigrationResult
      */
     public function run(): MigrationResult
     {
@@ -86,48 +84,46 @@ class AutogenerateBookingForm extends AbstractMigration
 
     /**
      * Auto generate event booking form.
-     *
-     * @throws Exception
      */
     private function autoGenerateBookingForm(): void
     {
         // Initialize the contao framework
         $this->framework->initialize();
+        $arrYaml = Yaml::parseFile($this->projectDir.'/vendor/markocupic/calendar-event-booking-bundle/sql/form-generator.yaml');
+        $arrForm = $arrYaml['form'];
+        $arrFormFields = $arrYaml['form']['form_fields'];
 
-        $sqlTlForm = file_get_contents($this->projectDir.'/vendor/markocupic/calendar-event-booking-bundle/sql/tl_form.sql');
-        $sqlTlFormField = file_get_contents($this->projectDir.'/vendor/markocupic/calendar-event-booking-bundle/sql/tl_form_field.sql');
+        // Create new form
+        $form = new FormModel();
+        $arrForm['tstamp'] = time();
+        $arrForm['title'] = \is_string($arrForm['title']) ? $this->encodeInput($arrForm['title']) : '';
 
-        // Set tstamp
-        $sqlTlForm = str_replace('##tstamp##', (string) time(), $sqlTlForm);
+        $form->setRow($arrForm);
+        $form->save();
 
-        // Insert into tl_form
-        $this->connection->executeStatement($sqlTlForm);
-
-        if (($intInsertId = $this->connection->lastInsertId()) > 0) {
-            // Set tl_form.isCalendarEventBookingForm to true if field exists
-            $schemaManager = $this->connection->getSchemaManager();
-            $columns = $schemaManager->listTableColumns('tl_form');
-
-            if (isset($columns['iscalendareventbookingform'])) {
-                $set = [
-                    'isCalendarEventBookingForm' => '1',
-                ];
-
-                $this->connection->update('tl_form', $set, ['id' => $intInsertId]);
+        foreach ($arrFormFields as $ff) {
+            $arrFormField = array_map(static fn ($value) => \is_array($value) ? serialize($value) : $value, $ff);
+            // Set class
+            if (isset($arrFormField['name'], $arrFormField['class']) && false !== strpos($arrFormField['class'], '%s')) {
+                $arrFormField['class'] = sprintf($arrFormField['class'], $arrFormField['name']);
             }
 
-            // Load dca tl_form_field
-            $this->controller->loadDataContainer('tl_form_field');
+            $arrFormField['pid'] = $form->id;
+            $arrFormField['tstamp'] = time();
 
-            $strExtensions = $GLOBALS['TL_DCA']['tl_form_field']['fields']['extensions']['default'] ?? '';
-
-            // Set pid & tstamp
-            $sqlTlFormField = str_replace('##pid##', $intInsertId, $sqlTlFormField);
-            $sqlTlFormField = str_replace('##tstamp##', (string) time(), $sqlTlFormField);
-            $sqlTlFormField = str_replace('##extensions##', $strExtensions, $sqlTlFormField);
-
-            // Insert into tl_form_field
-            $this->connection->executeStatement($sqlTlFormField);
+            // Create new form field
+            $formField = new FormFieldModel();
+            $formField->setRow($arrFormField);
+            $formField->save();
         }
+    }
+
+    private function encodeInput(string $varValue): string
+    {
+        $varValue = Input::decodeEntities($varValue);
+        $varValue = Input::xssClean($varValue, true);
+        $varValue = Input::stripTags($varValue);
+
+        return Input::encodeSpecialChars($varValue);
     }
 }
