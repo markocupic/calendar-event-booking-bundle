3f2e16ece74d8cdfd5c503acad57852e35a1d089
Marko Cupic <m.cupic@gmx.ch>
2025-06-15 12:42:33 +0200

    Upgrade ecs to version 6.13

--- .ecs/config/default.php
+++ .ecs/config/default.php
@@ -2,15 +2,30 @@
 
 declare(strict_types=1);
 
+use Contao\EasyCodingStandard\Set\SetList;
+use PhpCsFixer\Fixer\Comment\HeaderCommentFixer;
+use PhpCsFixer\Fixer\Whitespace\MethodChainingIndentationFixer;
+use SlevomatCodingStandard\Sniffs\Variables\UnusedVariableSniff;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Symplify\EasyCodingStandard\ValueObject\Option;
 
-return static function (ECSConfig  $ECSConfig): void {
-    // Contao
-    $ECSConfig->import(__DIR__ . '../../../../../contao/easy-coding-standard/config/contao.php');
-
-    // Custom
-    $ECSConfig->import(__DIR__.'/set/header_comment_fixer.php');
-
-    // Custom
-    $ECSConfig->import(__DIR__.'/set/skip_configuration.php');
-};
+return ECSConfig::configure()
+    ->withSets([SetList::CONTAO])
+    ->withPaths([
+        __DIR__ . '/../../src',
+    ])
+    ->withSkip([
+        MethodChainingIndentationFixer::class => [
+            '*/DependencyInjection/Configuration.php',
+        ],
+        UnusedVariableSniff::class => [
+            //'core-bundle/tests/Session/Attribute/ArrayAttributeBagTest.php',
+        ],
+    ])
+    ->withRootFiles()
+    ->withParallel()
+    ->withSpacing(Option::INDENTATION_SPACES, "\n")
+    ->withConfiguredRule(HeaderCommentFixer::class, [
+        'header' => "This file is part of Calendar Event Booking Bundle.\n\n(c) Marko Cupic <m.cupic@gmx.ch>\n@license MIT\nFor the full copyright and license information,\nplease view the LICENSE file that was distributed with this source code.\n@link https://github.com/markocupic/calendar-event-booking-bundle",
+    ])
+    ->withCache(sys_get_temp_dir() . '/ecs/markocupic/calendar-event-booking-bundle');
