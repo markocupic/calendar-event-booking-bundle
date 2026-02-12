b5ef666585d917fd2b2c57dbb444a2c1912b2801
Marko Cupic <m.cupic@gmx.ch>
2026-02-12 21:23:27 +0100

    Add markocupic/easy-coding-standard dependency

--- tools/ecs/config/default.php
+++ tools/ecs/config/default.php
@@ -20,7 +20,7 @@ use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 
 return ECSConfig::configure()
-    ->withSets([SetList::CONTAO])
+    ->withSets([SetList::CONTAO, \Markocupic\EasyCodingStandard\Set\SetList::MARKOCUPIC])
     ->withPaths([
         __DIR__.'/../../../src',
         __DIR__.'/../../../tests',
@@ -35,7 +35,7 @@ return ECSConfig::configure()
         MethodChainingIndentationFixer::class => [
             '*/DependencyInjection/Configuration.php',
         ],
-        UnusedVariableSniff::class => [
+        UnusedVariableSniff::class            => [
             // 'core-bundle/tests/Session/Attribute/ArrayAttributeBagTest.php',
         ],
     ])
@@ -45,5 +45,4 @@ return ECSConfig::configure()
     ->withConfiguredRule(HeaderCommentFixer::class, [
         'header' => "This file is part of the Calendar Event Booking Bundle.\n\n(c) Marko Cupic <m.cupic@gmx.ch>\n@license MIT\nFor the full copyright and license information,\nplease view the LICENSE file that was distributed with this source code.\n@link https://github.com/markocupic/calendar-event-booking-bundle",
     ])
-    ->withCache(sys_get_temp_dir().'/ecs_default_cache')
-;
+    ->withCache(sys_get_temp_dir().'/ecs_default_cache');
